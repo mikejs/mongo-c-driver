d09d5e66d7842268cbbb56da34aac33dae7c26c3
Michael Stephens <me@mikej.st>
2010-02-08 18:44:52 -0500

    functions for working with mongo's internal timestamp type

--- src/bson.h
+++ src/bson.h
@@ -59,6 +59,7 @@ typedef union{
 #pragma pack()
 
 typedef int64_t bson_date_t; /* milliseconds since epoch UTC */
+typedef int64_t bson_timestamp_t;
 
 /* ----------------------------
    READING
@@ -93,6 +94,7 @@ const char * bson_iterator_value( const bson_iterator * i );
 double bson_iterator_double( const bson_iterator * i );
 int bson_iterator_int( const bson_iterator * i );
 int64_t bson_iterator_long( const bson_iterator * i );
+bson_timestamp_t bson_iterator_timestamp( const bson_iterator * i );
 
 /* false: boolean false, 0 in any type, or null */
 /* true: anything else (even empty strings and objects) */
@@ -155,6 +157,7 @@ bson_buffer * bson_append_oid( bson_buffer * b , const char * name , const bson_
 bson_buffer * bson_append_new_oid( bson_buffer * b , const char * name );
 bson_buffer * bson_append_int( bson_buffer * b , const char * name , const int i );
 bson_buffer * bson_append_long( bson_buffer * b , const char * name , const int64_t i );
+bson_buffer * bson_append_timestamp( bson_buffer * b, const char * name , const bson_timestamp_t ts);
 bson_buffer * bson_append_double( bson_buffer * b , const char * name , const double d );
 bson_buffer * bson_append_string( bson_buffer * b , const char * name , const char * str );
 bson_buffer * bson_append_symbol( bson_buffer * b , const char * name , const char * str );
